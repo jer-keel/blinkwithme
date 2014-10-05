4c27ac28c792c4e39e97c8e7ae384d36893b80f0
kenjones21 <kenjones22193@aim.com>
2014-10-04 22:09:50 -0400

    Initial Core Commit of random crap - Jeremy
    Commit and then sync

---
+++ levels/level1.js
@@ -0,0 +1,11 @@
+/*  
+ *  Contains the spatial information for level 1
+ */
+
+ var level_1 = function(){
+    var title = 'Level 1 - Start';
+    var walls = [];
+    walls[0] = new wall(50,50,1,25);
+    walls[1] = new wall(50,50,0,25);
+ }
+
